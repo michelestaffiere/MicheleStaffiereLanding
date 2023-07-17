e37b06c3a6b8b988f6a27ddb0479f2107aae9799
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-07-16 23:21:55 -0400

    roughing in mobile layout out

--- Scripts/App.js
+++ Scripts/App.js
@@ -99,28 +99,14 @@ firstLoadGlitchAnimation();
 const contactButton = document.getElementById("contactMeBtn");
 const closeContactModalButton = document.getElementById("closeModal")
 const modal = document.getElementById("modal");
-const formSubmit = document.querySelector('.submit')
 
 contactButton.addEventListener('click',()=>{
     modal.showModal();
-});
+    modal.classList.remove('above');
+})
 
 closeContactModalButton.addEventListener('click',() =>{
+    modal.classList.add('above');
     modal.close();
-});
+})
 
-formSubmit.addEventListener('submit',(e) =>{
-    const nameInput = document.getElementById('full-name');
-    const emailInput = document.getElementById('email-address');
-    const messageInput = document.getElementById('message');
-    e.preventDefault();
-    console.log("clearing inputs")
-
-    nameInput.value="";
-    emailInput.value="";
-    messageInput.value="";
-
-    if(nameInput.value === "" && emailInput.value === ""  && messageInput.value === "" ){
-        console.log('inputs cleared!');
-    }
-});
