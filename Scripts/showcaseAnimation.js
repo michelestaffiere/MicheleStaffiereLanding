fe5fde01a1a03246bf460754198c1e42d98f6f31
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-08-01 17:26:59 -0400

    fixed footer logic

--- Scripts/showcaseAnimation.js
+++ Scripts/showcaseAnimation.js
@@ -2,18 +2,16 @@ const showCaseAnimation = (node) =>{
     window.addEventListener('scroll', ()=>{
         const scrollPosition = window.scrollY + window.innerHeight;
         const showCase = document.querySelector('.showCase');
-        const showCaseTop = showCase.offsetTop + 400;
-        const showCaseBottom = showCaseTop + showCase.offsetHeight +900;
+        const showCaseTop = showCase.offsetTop + 900;
+        const showCaseBottom = showCaseTop + showCase.offsetHeight + 100;
 
     
         if (scrollPosition > showCaseTop && scrollPosition < showCaseBottom){
             document.querySelector('.project1').style = "transform: translateX(0%); opacity: 1"
             document.querySelector('.project2').style = "transform: translateX(0%); opacity: 1"
-            // document.querySelector('.project3').style = "transform: translateX(0%); opacity: 1"
         } else{
             document.querySelector('.project1').style = "transform: translateX(100%); opacity: 0"
             document.querySelector('.project2').style = "transform: translateX(-100%); opacity: 0"
-            // document.querySelector('.project3').style = "transform: translateX(100%); opacity: 0"
         }
     });
 };
